e3952e03585eb304edd2275d6807a3cdca20dbaf
Bogdan Popa <bogdan@defn.io>
2021-11-17 12:32:35 +0200

    test: drop async tests
    The implementation was too sloppy and it allowed for orderings that
    led to false-negatives.
    
    Closes #1

--- resource-pool/test/data/pool.rkt
+++ resource-pool/test/data/pool.rkt
@@ -7,17 +7,10 @@
            rackunit
            rackunit/text-ui)
 
-  (define gen:timeout
-    (gen:integer-in 100 500))
-
   (define gen:command
     (gen:frequency
      `((5 . ,(gen:const `(take)))
-       (5 . ,(gen:let ([timeout gen:timeout])
-               (gen:const `(take/timeout ,timeout))))
        (5 . ,(gen:const `(release)))
-       (5 . ,(gen:let ([timeout gen:timeout])
-               (gen:const `(release-after ,timeout))))
        (1 . ,(gen:const `(expire)))
        (1 . ,(gen:const `(close))))))
 
@@ -25,8 +18,18 @@
     (gen:let ([commands (gen:list gen:command)])
       (append commands '((close)))))
 
-  (struct state (p size ttl stack thds pending expired closed?)
+  (struct state
+    (p           ;; pool
+     size        ;; pool size
+     ttl         ;; item ttl
+     stack       ;; stack-set of leased items
+     expired     ;; set of expired items
+     closed?     ;; has the pool been successfully closed?
+     )
     #:transparent)
+
+  (define (make-state p size ttl)
+    (state p size ttl null null #f))
 
   (define (box-update b f)
     (let loop ([v (unbox b)])
@@ -35,158 +38,71 @@
         [(box-cas! b v (f v)) new-v]
         [else (loop (unbox b))])))
 
-  (define-syntax-rule (thd e0 e ...)
-    (let ([ch (make-channel)])
-      (begin0 ch
-        (thread
-         (lambda ()
-           (with-handlers ([exn:fail? (λ (ex)
-                                        (channel-put ch ex))])
-             e0 e ...
-             (channel-put ch 'done)))))))
+  (define/match (interp _c s)
+    [('(take) (state p _size _ttl _stack _expired #t))
+     (begin0 s
+       (check-exn
+        #rx"pool closed"
+        (lambda ()
+          (pool-release! p (gensym)))))]
 
-  (define/match (interp s c)
-    [((state p size _ stack _ pending expired closed?)
-      '(take))
-     (cond
-       [closed?
-        (begin0 s
-          (check-exn
-           #rx"pool closed"
-           (lambda ()
-             (pool-release! p (gensym)))))]
+    [('(take) (state p size _ttl stack expired #f))
+     (define taken (length stack))
+     (define r (pool-take! p 100))
+     (when (< taken size)
+       (check-not-false r))
+     (when r
+       (check-false (memq r expired)))
+     (struct-copy state s [stack (if r (cons r stack) stack)])]
 
-       [else
-        (define taken
-          (+ (length stack)
-             (length (unbox pending))))
-        (define r (pool-take! p 100))
-        (when (< taken size)
-          (check-not-false r))
-        (when r
-          (check-false (memq r expired)))
-        (struct-copy state s [stack (if r (cons r stack) stack)])])]
+    [('(release) (state p _size _ttl '() _expired closed?))
+     (begin0 s
+       (check-exn
+        (if closed?
+            #rx"pool closed"
+            #rx"never leased")
+        (lambda ()
+          (pool-release! p (gensym)))))]
 
-    [((state p _ _ _ thds pending expired closed?)
-      `(take/timeout ,timeout))
-     (cond
-       [closed?
-        (struct-copy state s
-                     [thds (cons
-                            (thd
-                             (check-exn
-                              #rx"pool closed"
-                              (lambda ()
-                                (pool-take! p timeout))))
-                            thds)])]
+    [('(release) (state p _size _ttl stack _expired _closed?))
+     (pool-release! p (car stack))
+     (struct-copy state s [stack (cdr stack)])]
 
-       [else
-        (struct-copy state s
-                     [thds (cons
-                            (thd
-                             (define r (pool-take! p timeout))
-                             (when r
-                               (sync (system-idle-evt))
-                               (check-false (memq r (unbox pending)))
-                               (check-false (memq r expired))
-                               (pool-release! p r)))
-                            thds)])])]
+    [('(expire) (state p _size _ttl '() _expired closed?))
+     (begin0 s
+       (check-exn
+        (if closed?
+            #rx"pool closed"
+            #rx"never leased")
+        (lambda ()
+          (pool-release! p (gensym)))))]
 
-    [((state p _ _ stack _ _ _ closed?)
-      '(release))
-     (cond
-       [(null? stack)
-        (begin0 s
-          (check-exn
-           (if closed?
-               #rx"pool closed"
-               #rx"never leased")
-           (lambda ()
-             (pool-release! p (gensym)))))]
+    [('(expire) (state p _size ttl stack expired _closed?))
+     (define v (car stack))
+     (pool-release! p v)
+     (sync (alarm-evt (+ (current-inexact-milliseconds) (* 2 ttl))))
+     (sync (system-idle-evt))
+     (struct-copy state s
+                  [stack (cdr stack)]
+                  [expired (cons v expired)])]
 
-       [else
-        (pool-release! p (car stack))
-        (struct-copy state s [stack (cdr stack)])])]
+    [('(close) (state p _size _ttl _stack _expired #t))
+     (begin0 s
+       (check-exn
+        #rx"pool closed"
+        (lambda ()
+          (pool-close! p))))]
 
-    [((state p _ _ stack thds pending _ closed?)
-      `(release-after ,timeout))
-     (cond
-       [(null? stack)
-        (struct-copy state s [thds (cons
-                                    (thd
-                                     (check-exn
-                                      (if closed?
-                                          #rx"pool closed"
-                                          #rx"never leased")
-                                      (lambda ()
-                                        (sleep (/ timeout 1000.0))
-                                        (pool-release! p (gensym)))))
-                                    thds)])]
+    [('(close) (state p _size _ttl '() _expired #f))
+     (begin0 (struct-copy state s [closed? #t])
+       (pool-close! p))]
 
-       [else
-        (define v (car stack))
-        (box-update pending (λ (vs)
-                              (cons v vs)))
-        (struct-copy state s
-                     [stack (cdr stack)]
-                     [thds (cons
-                            (thd
-                             (sleep (/ timeout 1000.0))
-                             (pool-release! p v)
-                             (box-update pending (λ (vs)
-                                                   (remq v vs))))
-                            thds)])])]
-
-    [((state p _ ttl stack _ _ expired closed?)
-      '(expire))
-     (cond
-       [(null? stack)
-        (begin0 s
-          (check-exn
-           (if closed?
-               #rx"pool closed"
-               #rx"never leased")
-           (lambda ()
-             (pool-release! p (gensym)))))]
-
-       [else
-        (define v (car stack))
-        (pool-release! p v)
-        (sync (alarm-evt (+ (current-inexact-milliseconds)
-                            (* 2 ttl))))
-        (sync (system-idle-evt))
-        (struct-copy state s
-                     [stack (cdr stack)]
-                     [expired (cons v expired)])])]
-
-    [((state p _ _ stack thds pending _ closed?)
-      '(close))
-     (for ([ch (in-list thds)])
-       (define maybe-exn (sync ch))
-       (when (exn? maybe-exn)
-         (raise maybe-exn)))
-
-     (cond
-       [closed?
-        (begin0 (struct-copy state s [thds null])
-          (check-exn
-           #rx"pool closed"
-           (lambda ()
-             (pool-close! p))))]
-
-       [(and (null? stack)
-             (null? (unbox pending)))
-        (begin0 (struct-copy state s
-                             [thds null]
-                             [closed? #t])
-          (pool-close! p))]
-
-       [else
-        (begin0 (struct-copy state s [thds null])
-          (check-exn
-           #rx"attempted to close pool without releasing all the resources"
-           (lambda ()
-             (pool-close! p))))])])
+    [('(close) (state p _size _ttl _stack _expired #f))
+     (begin0 s
+       (check-exn
+        #rx"attempted to close pool without releasing all the resources"
+        (lambda ()
+          (pool-close! p))))])
 
   (define (interp* size ttl cs)
     (define p
@@ -196,9 +112,9 @@
        (let ([seq (box 0)])
          (lambda ()
            (box-update seq add1)))))
-    (for/fold ([s (state p size ttl null null (box null) null #f)])
+    (for/fold ([s (make-state p size ttl)])
               ([c (in-list cs)])
-      (interp s c)))
+      (interp c s)))
 
   (parameterize ([current-idle-timeout-slack 0])
     (run-tests
