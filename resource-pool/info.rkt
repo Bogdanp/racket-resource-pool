e236175a91cfaa30c4422e8186caa947dfd06d5c
Bogdan Popa <bogdan@defn.io>
2022-12-12 10:05:37 +0200

    build: add license to info.rkt files

--- resource-pool/info.rkt
+++ resource-pool/info.rkt
@@ -1,5 +1,6 @@
 #lang info
 
+(define license 'BSD-3-Clause)
 (define version "0.1")
 (define collection 'multi)
 (define deps '("base"
@@ -10,4 +11,3 @@
                      "resource-pool-lib"
                      "scribble-lib"))
 (define implies '("resource-pool-lib"))
-(define update-implies '("resource-pool-lib"))
